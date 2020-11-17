8857fdffa3c3dfbcefccf088d94f9612b08bf467
Isaack Mwenda <mwendaisaac@gmail.com>
2020-11-17 15:16:26 +0300

    remove episode 4-5 coding plans

--- configuration/coding_plans.py
+++ configuration/coding_plans.py
@@ -72,40 +72,6 @@ def get_rqa_coding_plans(pipeline_name):
                        )
                    ],
                    ws_code=CodeSchemes.WS_CORRECT_DATASET.get_code_with_match_value("OXFAM-WASH s01e03"),
-                   raw_field_fold_strategy=FoldStrategies.concatenate),
-
-        CodingPlan(raw_field="rqa_s01e04_raw",
-                   time_field="sent_on",
-                   run_id_field="rqa_s01e04_run_id",
-                   coda_filename="OXFAM_WASH_s01e04.json",
-                   icr_filename="s01e04.csv",
-                   coding_configurations=[
-                       CodingConfiguration(
-                           coding_mode=CodingModes.MULTIPLE,
-                           code_scheme=CodeSchemes.S01E04,
-                           coded_field="rqa_s01e04_coded",
-                           analysis_file_key="rqa_s01e04",
-                           fold_strategy=lambda x, y: FoldStrategies.list_of_labels(CodeSchemes.S01E04, x, y)
-                       )
-                   ],
-                   ws_code=CodeSchemes.WS_CORRECT_DATASET.get_code_with_match_value("OXFAM-WASH s01e04"),
-                   raw_field_fold_strategy=FoldStrategies.concatenate),
-
-        CodingPlan(raw_field="rqa_s01e05_raw",
-                   time_field="sent_on",
-                   run_id_field="rqa_s01e05_run_id",
-                   coda_filename="OXFAM_WASH_s01e05.json",
-                   icr_filename="s01e05.csv",
-                   coding_configurations=[
-                       CodingConfiguration(
-                           coding_mode=CodingModes.MULTIPLE,
-                           code_scheme=CodeSchemes.S01E05,
-                           coded_field="rqa_s01e05_coded",
-                           analysis_file_key="rqa_s01e05",
-                           fold_strategy=lambda x, y: FoldStrategies.list_of_labels(CodeSchemes.S01E05, x, y)
-                       )
-                   ],
-                   ws_code=CodeSchemes.WS_CORRECT_DATASET.get_code_with_match_value("OXFAM-WASH s01e05"),
                    raw_field_fold_strategy=FoldStrategies.concatenate)
     ]
 

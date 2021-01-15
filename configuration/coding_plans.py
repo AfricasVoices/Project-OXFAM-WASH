e7ef64dd89e7c837b8c767e24b22ad3c61290f00
Isaack Mwenda <mwendaisaac@gmail.com>
2021-01-15 10:14:53 +0300

    handle accountability question as rqa

--- configuration/coding_plans.py
+++ configuration/coding_plans.py
@@ -74,6 +74,26 @@ def get_rqa_coding_plans(pipeline_name):
                    ws_code=CodeSchemes.WS_CORRECT_DATASET.get_code_with_match_value("OXFAM-WASH s01e03"),
                    raw_field_fold_strategy=FoldStrategies.concatenate),
 
+        CodingPlan(raw_field="rqa_s01_accountability_raw",
+                   time_field="sent_on",
+                   coda_filename="OXFAM_WASH_s01_Accountability.json",
+                   run_id_field="rqa_s01_accountability_run_id",
+                   icr_filename="oxfam_accountability.csv",
+                   coding_configurations=[
+                       CodingConfiguration(
+                           coding_mode=CodingModes.MULTIPLE,
+                           code_scheme=CodeSchemes.S01_ACCOUNTABILITY,
+                           coded_field="rqa_s01_accountability_coded",
+                           analysis_file_key="rqa_s01_accountability",
+                           fold_strategy=lambda x, y: FoldStrategies.list_of_labels(
+                               CodeSchemes.S01_ACCOUNTABILITY, x,
+                               y)
+                       )
+                   ],
+                   ws_code=CodeSchemes.WS_CORRECT_DATASET.get_code_with_match_value(
+                       "OXFAM WASH s01 Accountability"),
+                   raw_field_fold_strategy=FoldStrategies.concatenate),
+
         CodingPlan(raw_field="oxfam_beneficiary_consent_raw",
                    time_field="sent_on",
                    run_id_field="oxfam_beneficiary_consent_run_id",
@@ -210,24 +230,6 @@ def get_follow_up_coding_plans(pipeline_name):
                    ],
                    ws_code=CodeSchemes.WS_CORRECT_DATASET.get_code_with_match_value(
                        "OXFAM WASH s01 Programme Evaluation"),
-                   raw_field_fold_strategy=FoldStrategies.concatenate),
-
-        CodingPlan(raw_field="rqa_s01_accountability_raw",
-                   time_field="sent_on",
-                   coda_filename="OXFAM_WASH_s01_Accountability.json",
-                   coding_configurations=[
-                       CodingConfiguration(
-                           coding_mode=CodingModes.MULTIPLE,
-                           code_scheme=CodeSchemes.S01_ACCOUNTABILITY,
-                           coded_field="rqa_s01_accountability_coded",
-                           analysis_file_key="rqa_s01_accountability",
-                           fold_strategy=lambda x, y: FoldStrategies.list_of_labels(
-                               CodeSchemes.S01_ACCOUNTABILITY, x,
-                               y)
-                       )
-                   ],
-                   ws_code=CodeSchemes.WS_CORRECT_DATASET.get_code_with_match_value(
-                       "OXFAM WASH s01 Accountability"),
                    raw_field_fold_strategy=FoldStrategies.concatenate)
     ]
 
